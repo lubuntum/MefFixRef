bc23fb632ab3ee6545b0c5ea190be5e028f784e1
lubuntum <lubuntum@gmail.com>
2022-02-16 00:18:26 +0700

    Сделал начальные настройки для приложения, реализовал начальную логику для SettingsViewModel и SettingsFragment

--- app/src/main/java/com/example/memfixref/ui/mainfragments/settings/SettingsFragment.java
+++ app/src/main/java/com/example/memfixref/ui/mainfragments/settings/SettingsFragment.java
@@ -1,25 +1,58 @@
 package com.example.memfixref.ui.mainfragments.settings;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.memfixref.R;
 
 public class SettingsFragment extends Fragment {
+    private SettingsViewModel settingsViewModel;
+    private Switch themeSwitchBtn;
+    private Switch statisticsSwitchBtn;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        settingsViewModel = new ViewModelProvider(getActivity()).get(SettingsViewModel.class);
         return inflater.inflate(R.layout.fragment_settings,container,false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        themeSwitchBtn = view.findViewById(R.id.themeSwitchBtn);
+        statisticsSwitchBtn = view.findViewById(R.id.statisticSwitchBtn);
+
+        if (settingsViewModel.getTheme().equals(SettingsViewModel.BLACK_THEME))
+            themeSwitchBtn.setChecked(true);
+        statisticsSwitchBtn.setChecked(settingsViewModel.isStatisticsAllowed());
+
+        themeSwitchBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isAllow) {
+                if (isAllow)
+                    settingsViewModel.setTheme(SettingsViewModel.BLACK_THEME);
+                else
+                    settingsViewModel.setTheme(SettingsViewModel.WHITE_THEME);
+                //Log.d("THEME",settingsViewModel.getTheme());
+            }
+
+        });
+        statisticsSwitchBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isAllow) {
+                settingsViewModel.setStatistics(isAllow);
+                //Log.d("USE_STATISTICS",String.valueOf(settingsViewModel.isStatisticsAllowed()));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/memfixref/ui/mainfragments/settings/SettingsViewModel.java
+++ app/src/main/java/com/example/memfixref/ui/mainfragments/settings/SettingsViewModel.java
@@ -1,4 +1,44 @@
 package com.example.memfixref.ui.mainfragments.settings;
 
-public class SettingsViewModel {
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.ViewModel;
+
+public class SettingsViewModel extends AndroidViewModel {
+    public static final String SETTINGS_STORAGE = "AppSettings";
+
+    public static final String THEME = "ThemeKey";
+    public static final String BLACK_THEME = "Black";
+    public static final String WHITE_THEME = "White";//Тут можно сделать путь до реальной темы
+
+    public static final String STATISTICS = "StatisticsKey";
+
+    private SharedPreferences settingsPreferences;
+    private SharedPreferences.Editor editor;
+    public SettingsViewModel(Application app){
+        super(app);
+        settingsPreferences = getApplication().getSharedPreferences(
+                SETTINGS_STORAGE, Context.MODE_PRIVATE);
+        editor = settingsPreferences.edit();
+    }
+    public void setTheme(String theme){
+        editor.putString(THEME,theme);
+        editor.apply();
+    }
+    //Если нет то по дефолту белая
+    public String getTheme(){
+        return settingsPreferences.getString(THEME,WHITE_THEME);
+    }
+    public void setStatistics(boolean isAllowed){
+        editor.putBoolean(STATISTICS,isAllowed);
+        editor.apply();
+    }
+    //Если нет по дефолту true
+    public boolean isStatisticsAllowed(){
+        return settingsPreferences.getBoolean(STATISTICS,true);
+    }
+
 }
