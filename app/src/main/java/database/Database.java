01c52653894bbd0a2c5854487a15a030ee4a73d1
lubuntum <lubuntum@gmail.com>
2022-03-20 15:39:34 +0700

    Удалил все миграции и изменил session_id на autoincrement, слишком много лишних миграций для разработки, пересоздал бд с новой чистой схемой

--- app/src/main/java/database/Database.java
+++ app/src/main/java/database/Database.java
@@ -16,7 +16,7 @@ import database.entities.Kit;
 import database.entities.Session;
 
 
-@androidx.room.Database(entities = {Kit.class, Cell.class, Session.class},version = 2)
+@androidx.room.Database(entities = {Kit.class, Cell.class, Session.class},version = 1)
 public abstract class Database extends RoomDatabase {
     private static final String DATABASE_NAME = "AppDatabase";
     public abstract KitDao kitDao();
@@ -24,6 +24,7 @@ public abstract class Database extends RoomDatabase {
     public abstract SessionDao sessionDao();
     public static volatile Database INSTANCE = null;
 
+    /*
     public static final Migration MIGRATION_1_2 = new Migration(1,2) {
         @Override
         public void migrate(@NonNull SupportSQLiteDatabase database) {
@@ -37,13 +38,27 @@ public abstract class Database extends RoomDatabase {
                     "ADD COLUMN total_time INTEGER DEFAULT 0 NOT NULL;");
         }
     };
+    public static final Migration MIGRATION_2_3 = new Migration(2,3) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            database.beginTransaction();
+            database.execSQL("ALTER TABLE session RENAME TO session_old");
+            database.execSQL("CREATE TABLE session (" +
+                    "id INTEGER primary key autoincrement NOT NULL," +
+                    "kit_id integer NOT NULL REFERENCES kit(id))");
+            database.endTransaction();
+        }
+    };
+     */
+
 
     public static Database getInstance(Context context){
         if(INSTANCE == null){
             synchronized (Database.class){
                 if (INSTANCE == null){
                     INSTANCE = Room.databaseBuilder(context,Database.class,DATABASE_NAME)
-                            .addMigrations(MIGRATION_1_2)
+                            //.addMigrations(MIGRATION_1_2)
+                            .fallbackToDestructiveMigration()
                             .build();
                 }
             }
