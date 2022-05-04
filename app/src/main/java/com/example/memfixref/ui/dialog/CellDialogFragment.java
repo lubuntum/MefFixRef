20a6af6d80c46cd774d0f16f4427a42b29de6799
lubuntum <lubuntum@gmail.com>
2022-05-05 03:47:34 +0700

    Реализовал сохранение записи в cell.recordPath по факту, так же если нету прав то они будут обязательно запрошены, иначе нельзя

--- app/src/main/java/com/example/memfixref/ui/dialog/CellDialogFragment.java
+++ app/src/main/java/com/example/memfixref/ui/dialog/CellDialogFragment.java
@@ -1,12 +1,24 @@
 package com.example.memfixref.ui.dialog;
 
+import static android.Manifest.permission.MANAGE_EXTERNAL_STORAGE;
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.RECORD_AUDIO;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+import static com.example.memfixref.MainActivity.REQUEST_AUDIO_PERMISSION_CODE;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,26 +29,38 @@ import android.widget.Toast;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.DialogFragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.beardedhen.androidbootstrap.BootstrapButton;
+import com.example.memfixref.MainActivity;
 import com.example.memfixref.R;
 import com.example.memfixref.ui.mainfragments.kit.onekitdata.KitViewModel;
 
 import org.jetbrains.annotations.NotNull;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.sql.Timestamp;
 
 import database.entities.Cell;
 import services.BitmapScaler;
 
 public class CellDialogFragment extends DialogFragment {
+    //UI
     private EditText cellKeyEditText;
     private EditText cellValueEditText;
     private BootstrapButton confirmBtn;
     private BootstrapButton removeCellBtn;
     private ImageButton addImageBtn;
+    private ImageButton addRecordBtn;
+
+    //Record audio
+    public Boolean isMicOn = false;
+    private MediaRecorder mRecorder;
+    //Image
     private ActivityResultLauncher<Intent> loadImageResultLauncher;
 
     private Cell cell;
@@ -90,7 +114,11 @@ public class CellDialogFragment extends DialogFragment {
         cellValueEditText = view.findViewById(R.id.cellValue);
         confirmBtn = view.findViewById(R.id.confirmBtn);
         removeCellBtn = view.findViewById(R.id.removeCellBtn);
+
         addImageBtn = view.findViewById(R.id.addImageBtn);
+        addRecordBtn = view.findViewById(R.id.addRecordBtn);
+
+        mRecorder = new MediaRecorder();
 
         //Если редактируется уже существующая
         if (getArguments() != null && getArguments().containsKey("index")){
@@ -104,6 +132,46 @@ public class CellDialogFragment extends DialogFragment {
             cell = new Cell("key","value");
             kitViewModel.getKit().cells.add(cell);
         }
+        addRecordBtn.setOnClickListener((View v)->{
+
+            if (checkPermissions()) {
+                if (isMicOn) {
+                    addRecordBtn.setImageResource(R.drawable.ic_mic_off);
+                    mRecorder.stop();
+                    mRecorder.release();
+                } else {
+                    addRecordBtn.setImageResource(R.drawable.ic_mic_on);
+                    if (cell.recordPath == null || cell.recordPath.equals("")) {
+                        //Если запись происходит впервые, то создать имя файла(имя = дата первого создания)
+                        cell.recordPath = Environment.getExternalStorageDirectory().getPath();
+                        cell.recordPath += "/" + new Timestamp(System.currentTimeMillis()).getTime() + ".3gp";
+                    }
+
+                    mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+                    mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+                    mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+                    mRecorder.setOutputFile(cell.recordPath);
+
+                    try {
+                        mRecorder.prepare();
+
+                        mRecorder.start();
+                    } catch (IOException e) {
+                        Log.d("Error", e.getMessage());
+                        Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+                isMicOn = !isMicOn;
+            }
+            else {
+                requestPermissions();
+            }
+
+
+
+        });
 
         addImageBtn.setOnClickListener((View v)->{
             Toast.makeText(getContext(),"Show Gallery",Toast.LENGTH_SHORT).show();
@@ -144,5 +212,16 @@ public class CellDialogFragment extends DialogFragment {
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+    }
+    public boolean checkPermissions() {
+        // this method is used to check permission
+        int result = ContextCompat.checkSelfPermission(getContext(), WRITE_EXTERNAL_STORAGE);
+        int result1 = ContextCompat.checkSelfPermission(getContext(), RECORD_AUDIO);
+        return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED;
+    }
+    private void requestPermissions() {
+        // this method is used to request the
+        // permission for audio recording and storage.
+        ActivityCompat.requestPermissions(getActivity(), new String[]{RECORD_AUDIO, WRITE_EXTERNAL_STORAGE}, REQUEST_AUDIO_PERMISSION_CODE);
     }
 }
