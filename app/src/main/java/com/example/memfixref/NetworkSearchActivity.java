52f309bed2c52e218a51f781372ed208b8cd7cc5
lubuntum <lubuntum@gmail.com>
2022-06-12 02:45:36 +0700

    Добавил новую активити для поиска наборов и подгружаемый фрагмент для поиска наборов

---
+++ app/src/main/java/com/example/memfixref/NetworkSearchActivity.java
@@ -0,0 +1,14 @@
+package com.example.memfixref;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NetworkSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_network);
+    }
+}
---
+++ app/src/main/java/com/example/memfixref/ui/mainfragments/kit/network/search/KitSearchFragment.java
@@ -0,0 +1,25 @@
+package com.example.memfixref.ui.mainfragments.kit.network.search;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.memfixref.R;
+
+public class KitSearchFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_network_search,container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
