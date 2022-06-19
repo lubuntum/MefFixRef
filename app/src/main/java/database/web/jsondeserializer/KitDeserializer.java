5ecb52f3b29e3793e77978dd551595aa09e3c171
lubuntum <lubuntum@gmail.com>
2022-06-19 17:42:37 +0700

    Добавлены парсеры для Kit & List<Kit>

--- app/src/main/java/database/web/jsondeserializer/KitDeserializer.java
+++ app/src/main/java/database/web/jsondeserializer/KitDeserializer.java
@@ -1,17 +1,39 @@
 package database.web.jsondeserializer;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 
 import java.lang.reflect.Type;
+import java.util.LinkedList;
 
+import database.entities.Cell;
 import database.entities.Kit;
 
 public class KitDeserializer implements JsonDeserializer<Kit> {
     @Override
     public Kit deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-        return null;
+        JsonObject jsonObject = json.getAsJsonObject();
+        JsonObject data = jsonObject.get("data").getAsJsonObject();
+
+        JsonObject kitJsonObject = data.get("kit").getAsJsonObject();
+        String name = kitJsonObject.get("kit_name").getAsString();
+        float rating = kitJsonObject.get("rating").getAsFloat();
+        Kit kit = new Kit(name,rating);
+
+        JsonArray cellJsonArray = data.get("cells").getAsJsonArray();
+        if (cellJsonArray != null) {
+            kit.cells = new LinkedList<>();
+            for (JsonElement cellJsonElement : cellJsonArray) {
+                JsonObject cellJsonObject = cellJsonElement.getAsJsonObject();
+                String key = cellJsonObject.get("key").getAsString();
+                String value = cellJsonObject.get("value").getAsString();
+                kit.cells.add(new Cell(key, value));
+            }
+        }
+        return kit;
     }
 }
--- app/src/main/java/database/web/jsondeserializer/KitListDeserializer.java
+++ app/src/main/java/database/web/jsondeserializer/KitListDeserializer.java
@@ -1,11 +1,14 @@
 package database.web.jsondeserializer;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 
 import java.lang.reflect.Type;
+import java.util.LinkedList;
 import java.util.List;
 
 import database.entities.Kit;
@@ -13,6 +16,15 @@ import database.entities.Kit;
 public class KitListDeserializer implements JsonDeserializer<List<Kit>> {
     @Override
     public List<Kit> deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-        return null;
+        List<Kit> kits = new LinkedList<>();
+        JsonObject jsonObject = json.getAsJsonObject();
+        JsonArray kitJsonArray = jsonObject.get("data").getAsJsonArray();
+        for(JsonElement kitJsonElement:kitJsonArray){
+            JsonObject kitJsonObject = kitJsonElement.getAsJsonObject();
+            String name = kitJsonObject.get("kit_name").getAsString();
+            float rating = kitJsonObject.get("rating").getAsFloat();
+            kits.add(new Kit(name, rating));
+        }
+        return kits;
     }
 }
